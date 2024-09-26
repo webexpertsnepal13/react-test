a08b5d178cd1bdf96a996ff710efeaf2e9576e05
SabinWEN <sharma.sabin@webexpertsnepal.com>
2024-09-26 11:52:01 +0545

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // basePath: '/nextjs',
+    basePath: '/nextjs',
     // assetPrefix: '/nextjs/', // Prefix static assets with the basePath
   };
   
