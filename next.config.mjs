c53a1760e6d7946c3f2ca930b06ea215d41021e2
SabinWEN <sharma.sabin@webexpertsnepal.com>
2024-09-26 09:51:30 +0545

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = {};
+module.exports = {
     basePath: '/nextjs',
-};
-
-module.exports = nextConfig;
+  }
+export default nextConfig;
